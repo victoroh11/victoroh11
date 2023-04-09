0a7643b497b4aa2a1f4c564a0fee7bd40bad667e
victoroh11 <94450857+victoroh11@users.noreply.github.com>
2023-04-09 17:50:00 +0800

    Delete oper.h

--- oper.h
+++
@@ -1,9 +0,0 @@
-// oper.h
-#include <string>
-#ifndef OPER_H
-#define OPER_H
-
-void save_game(std::string file_name, std::string player_name, int player_level, int player_health);
-void load_game(std::string file_name);
-
-#endif
